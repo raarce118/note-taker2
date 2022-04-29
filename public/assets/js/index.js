4d8f0031074d3af6e60e1440015a6436a0f21886
Rich Arce <raarce118@gmail.com>
2022-04-28 21:55:35 -0700

    test

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,5 +1,3 @@
-// Copy and pasted from starter code
-
 let noteTitle;
 let noteText;
 let saveNoteBtn;
@@ -60,6 +58,7 @@ const renderActiveNote = () => {
     noteText.setAttribute('readonly', true);
     noteTitle.value = activeNote.title;
     noteText.value = activeNote.text;
+    console.log(activeNote.title)
   } else {
     noteTitle.removeAttribute('readonly');
     noteText.removeAttribute('readonly');
